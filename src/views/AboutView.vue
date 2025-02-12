b266c0190ae4c04268874fe9399887da1a626d28
Manish Kumar <manish@qartsolutions.com>
2025-02-13 02:45:47 +0530

    initial commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="about">
+    <h1>About This Template</h1>
+    <p>This is a MEVN stack template that provides a solid foundation for building full-stack web applications.</p>
+    <p>Features included:</p>
+    <ul>
+      <li>Vue 3 with Composition API</li>
+      <li>Vuex for state management</li>
+      <li>Vue Router for navigation</li>
+      <li>Express.js backend with MongoDB</li>
+      <li>RESTful API structure</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutView'
+}
+</script>
+
+<style scoped>
+.about {
+  padding: 20px;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  margin: 10px 0;
+  font-size: 1.1em;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <h1>Welcome to MEVN Stack Template</h1>
+    <p>This is a starter template for building full-stack applications with:</p>
+    <ul>
+      <li>MongoDB</li>
+      <li>Express.js</li>
+      <li>Vue.js</li>
+      <li>Node.js</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView'
+}
+</script>
+
+<style scoped>
+.home {
+  padding: 20px;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  margin: 10px 0;
+  font-size: 1.2em;
+}
+</style>
